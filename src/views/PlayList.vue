ec940b26b3a4f1d0a65c3020d500c0877e32f220
linxuekai <linxuekai@qq.com>
2020-02-07 05:59:48 +0800

    调整样式,修正歌词

--- src/views/PlayList.vue
+++ src/views/PlayList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="player-wrap">
     <Aplayer
       class="player"
       :showLrc="true"
@@ -7,7 +7,7 @@
       :music="aplayerList[0]"
       :autoplay="true"
       :list="aplayerList"
-      listMaxHeight="100vh"
+      listMaxHeight="70vh"
     />
   </div>
 </template>
@@ -36,7 +36,7 @@ export default class VueComp extends Vue {
   get aplayerList (): AplayerMusic[] {
     return this.playlist.songs.map(song => ({
       src: song.url,
-      title: song.name,
+      title: song.name.replace(/周杰伦 - |\.mp3|\d+\.? ?/g, ''),
       artist: '周杰伦',
       pic: this.playlist.folderImg,
       lrc: song.url.replace('.mp3', '.lrc')
@@ -45,8 +45,15 @@ export default class VueComp extends Vue {
 }
 </script>
 
-<style lang="stylus">
-.player {
+<style lang="stylus" scoped>
+.player-wrap
 
-}
+.player
+  margin 18vh auto 10vh
+  box-shadow 3px 12px 35px rgba(130, 130, 130, .4)
+  max-width 500px
+
+/deep/ .aplayer-list ol li .aplayer-list-index
+  min-width 14px
+  text-align center
 </style>
